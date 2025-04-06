afe80d7ed0ac109fc63d64149f812b2ee58dd8e2
euclides <euclidesfreiresa@gmail.com>
2025-04-06 15:57:23 -0300

    init

---
+++ src/main/java/bit/turing/decide/DecideApplication.java
@@ -0,0 +1,13 @@
+package bit.turing.decide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bit/turing/decide/DecideApplicationTests.java
@@ -0,0 +1,13 @@
+package bit.turing.decide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
